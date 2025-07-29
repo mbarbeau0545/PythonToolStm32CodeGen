5ee531e5e0d1c74fdd949fb4b067067be5c28c5d
mbarbeau0545 <m.barbeau0545@gmail.com>
2025-07-29 19:59:35 +0200

    change name of variable

--- FMK_CodeGen/FMKCDA_CodeGen.py
+++ FMK_CodeGen/FMKCDA_CodeGen.py
@@ -196,7 +196,7 @@ class FMKCDA_CodeGen():
         var_other_cfg   += "    /**< Variable for Interna Sensors configuration*/\n"
         var_other_cfg   += f"    const t_sFMKCDA_ADcInternalSnsCfg c_FmkCda_HwInternalSnsCfg_as[{ENUM_ADC_INTERN_SENSOR}_NB] =" + "{\n"
         var_other_calib += "    /**< Variable for Internal Sensors Calibration address */\n"
-        var_other_calib += "    const volatile t_uint16* c_FmkCda_HwInternalSnsAddress_pas16[FMKCDA_ADC_INTERN_NB] = {\n"
+        var_other_calib += "    const volatile t_uint16* c_FmkCda_HwInternalSnsAddress_pau16[FMKCDA_ADC_INTERN_NB] = {\n"
         for idx, other_calib_info in enumerate(calib_astr):
             def_other_calib += f"    #define {ENUM_ADC_INTERN_SENSOR}_{other_calib_info[0]}_ADDRESS ((volatile t_uint16*){str(other_calib_info[1])})\n"
 
